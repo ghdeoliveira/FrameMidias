f07e10840d16b8137e834e8f99522aac965ccd59
Gabriel Henrique <gh.deoliveira@outlook.com>
2023-06-01 19:32:38 -0300

    commit

--- app/src/main/java/com/example/framemidias/fragments/audioFragment.java
+++ app/src/main/java/com/example/framemidias/fragments/audioFragment.java
@@ -1,28 +1,25 @@
 package com.example.framemidias.fragments;
 
+import static androidx.core.content.ContextCompat.getSystemService;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
+
+import androidx.fragment.app.Fragment;
 
 import com.example.framemidias.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link audioFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class audioFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -30,15 +27,6 @@ public class audioFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment audioFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static audioFragment newInstance(String param1, String param2) {
         audioFragment fragment = new audioFragment();
         Bundle args = new Bundle();
@@ -55,6 +43,7 @@ public class audioFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
@@ -63,4 +52,6 @@ public class audioFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_audio, container, false);
     }
+
+
 }
